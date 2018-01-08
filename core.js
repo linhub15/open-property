8610f4e2efe9434c2fcef152183543645a614de1
Hubert Lin <linhub15@gmail.com>
2018-01-08 09:54:38 -0700

    suiteExistsLogic

--- core.js
+++ core.js
@@ -1,12 +1,45 @@
 $(document).ready(function () {
   $("#submitButton").click(function() {
     // Take the text box and store input
-    var suite = "";
-    var houseNumber ="";
+    var addressArray = $("#searchQuery").value.split(" ");
+    var suite = getSuite(addressArray);
+    var houseNumber = getHouseNumber(addressArray);
     var apiUrl = assembleApiUrl(suite,houseNumber);
     dataApiCall(apiUrl);
   })
 })
+
+function getSuite(addressArray){
+  if (suiteExists(addressArray))
+  {
+    return addressArray[0];
+  }
+}
+
+function getHouseNumber(addressArray){
+  if (suiteExists(addressArray))
+  {
+    return addressArray[1];
+  }
+  else 
+  {
+    return addressArray[0];
+  }
+}
+
+function suiteExists(addressArray){
+  var tmpSuite = parseInt(addressArray[0]);
+  var tmpHouse = parseInt(addressArray[1]);
+  var returnValue;
+  if (typeof(tmpSuite) == "number" && typeof(tmpHouse) == "number") {}
+    returnValue = true;
+  }
+  else {
+    returnValue = false;
+  }
+  return returnValue;
+}
+
 
 function assembleApiUrl(suite, houseNumber) {
   var dataSetUrl = "https://data.edmonton.ca/resource/3pdp-qp95.json";
