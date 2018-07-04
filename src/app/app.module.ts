afe68741bf0c7eb77a4f51bdcf7f68cb7861e16a
Hubert Lin - Macbook <linhub15@gmail.com>
2018-07-03 23:18:02 -0600

    Implement UI - Toolbar & Add button to Address search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatInputModule, MatCardModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatToolbarModule, MatButtonModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { AddressSearchComponent } from './address-search/address-search.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddressSearchComponent
+    AddressSearchComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
