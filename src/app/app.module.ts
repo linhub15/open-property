81b98006bef1fae8f152692979a0abd1bd3f7b74
Hubert Lin - Macbook <linhub15@gmail.com>
2018-07-12 12:46:38 -0600

    Create AssessmentComponent to display Assessment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { MatInputModule, MatCardModule, MatToolbarModule, MatButtonModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import { AddressSearchComponent } from './address-search/address-search.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AssessmentComponent } from './assessment/assessment.component';
+
+
+const appRoutes: Routes = [
+  { path: 'address-search', component: AddressSearchComponent },
+  { path: 'results', component: AssessmentComponent },
+  { path: '', redirectTo: '/address-search', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AddressSearchComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    AssessmentComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     MatInputModule,
     MatCardModule,
     MatToolbarModule,
