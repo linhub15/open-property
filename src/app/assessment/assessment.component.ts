dbe019030c87aec0ae7fec465ad27c6f9f5cc9db
Hubert Lin - Macbook <linhub15@gmail.com>
2018-07-12 12:53:30 -0600

    Databind Search input to AssessmentComponent

--- src/app/assessment/assessment.component.ts
+++ src/app/assessment/assessment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-assessment',
@@ -9,6 +9,10 @@ export class AssessmentComponent implements OnInit {
 
   constructor() { }
 
+  @Input() suite: number;
+  @Input() house: number;
+  @Input() street: string;
+  
   ngOnInit() {
   }
 
