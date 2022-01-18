ba8ea79d28559f2867d91aad638ba9ff81d61770
Hubert Lin <linhub15@gmail.com>
2022-01-17 21:57:30 -0700

    chore: update angular packages to 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ const appRoutes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' }),
     GoogleMapsModule,
     MatInputModule,
     MatCardModule,
